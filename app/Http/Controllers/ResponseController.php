d6d70f268f61d0649bf3817919eb0781028116b0
Evan Hsu <evanhsu@gmail.com>
2017-04-18 00:05:33 -0600

    Added response array to ResponseController

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -5,13 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
+
 class ResponseController extends Controller
 {
-    //
+    protected $answers = [
+    	'ping'			=> 'OK',
+    	'phone'			=> '541-728-3826',
+    	'resume'		=> 'www.smirksoftware.com/resume.pdf',
+    	'puzzle'		=> ' ABCD\nA=>>>\nB<=<<\nC<>=>\nD<><=',
+    	'email address'	=> 'evanhsu@gmail.com',
+    	'degree'		=> 'BSEE - University of Michigan College of Engineering',
+    	'source'		=> 'https://github.com/evanhsu/balihooapi',
+    	'name'			=> 'Evan Hsu',
+    	'referrer'		=> 'whoishiring.io',
+    	'position'		=> 'Software Engineer',
+    	'years'			=> 'Since 2003',
+    	'status'		=> 'Yes',
+    ];
+
     public function index(Request $request)
     {
+    	this->logRequest();
+
+    	q = strtolower(urldecode($request->get('q')));
+    	d = strtolower(urldecode($request->get('d')));
+
+    	return response($this->answers[q], 200);
+    }
+
+    public function logRequest()
+    {
     	Log::info("=============================");
-    	Log::info(var_export($request, true));
-    	return response('message received', 200);
+    	Log::info($request->fullUrl());
     }
 }
